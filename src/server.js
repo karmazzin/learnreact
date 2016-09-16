efd4a21929d80e9c21e899c2c17ff09ff02a135b
Anton Karmazin <anton.karmazin@umbrella-web.com>
2016-09-16 21:51:01 +0300

    add basic server

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express  from 'express';
+
+const app = express();
+
+app.use((req, res) => {
+  res.end('<p>Hello World!</p>');
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on: ${PORT}`);
+});
+
